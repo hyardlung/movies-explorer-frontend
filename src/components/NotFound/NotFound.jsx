086e441a6a445977a6a1114e6ba703c228c4abc1
Dmitry Volkov <hyardlung@gmail.com>
2021-06-18 15:59:14 +0700

    feat: добавить страницу 404
    Сверстал и заадаптировал страницу, на которую пользователя редиректит
    при попытке перейти на несуществующий эндпоинт. Возврат на предыдущую
    страницу производится через хук useHistory.

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './NonFound.css';
 
 const NotFound = () => {
-  return (
-      <section className="not-fount">
-        <div className="not-fount__container">
+  const history = useHistory();
 
+  return (
+      <section className="not-found">
+        <div className="not-found__container">
+          <h1 className="not-found__title">404</h1>
+          <p className="not-found__text">Страница не найдена</p>
+          <span
+              onClick={() => history.goBack()}
+              className="not-found__go-back">
+            Назад
+          </span>
         </div>
       </section>
   );
