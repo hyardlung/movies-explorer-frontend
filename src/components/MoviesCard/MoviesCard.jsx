45e4204e8a4817ed1a75576debbb4285766893d1
Dmitry Volkov <hyardlung@gmail.com>
2021-07-09 20:35:07 +0700

    feat: добавить фильмам ссылки на трейлер
    Теперь по клику на постер фильма в новой вкладке открывается трейлер
    фильма на youtube. Также поправил стили самой карточки фильма.

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -45,10 +45,15 @@ const MoviesCard = ({
 
   return (
       <li key={movie.id} className="movies-card">
-        <img src={movieImage}
-             alt={movieTitle}
-             className="movies-card__img"
-        />
+        <a href={movieTrailer}
+           rel="noopener noreferrer"
+           target="_blank"
+           className="movie-card__trailer-link">
+          <img src={movieImage}
+               alt={movieTitle}
+               className="movies-card__img"
+          />
+        </a>
         <div className="movies-card__description-wrapper">
           <p className="movies-card__title">{movieTitle}</p>
           <span className="movies-card__duration">{movieDuration}</span>
